bf997c3055af82772145f5025542f75b63d19226
Holger Eudenbach <holger@eude.rocks>
2022-02-18 09:06:04 +0100

    test

--- ceph/rados_s3_bucket_status.sh
+++ ceph/rados_s3_bucket_status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # H.Eudenbach in 2022 - holger@eude.rocks
-# Local check for checkmk 2.0p19
+# Local check for checkmk 2.0p19, ceph 14
 #
 # place on rados gateway at -> /usr/lib/check_mk_agent/plugins/
 # make executeable -> chmod +x 
@@ -25,13 +25,13 @@ radosgw-admin bucket stats --bucket=$bucket | grep '"max\_size":' | sed 's/\"max
 radosgw_s3_size_remaining=$(($radosgw_s3_size_max - $radosgw_s3_size_actual))
 
 # calculating to GB - edit for other formats
-act=$(($radosgw_s3_size_actual / 1024 / 1024 / 1024)) #Actual Usage of Bucket
-max=$(($radosgw_s3_size_max / 1024 / 1024 / 1024)) #Maximum Usage of Bucket (Quota)
-rem=$(($radosgw_s3_size_remaining / 1024 / 1024 / 1024)) #Remaining Usage of Bucket
+act=$(($radosgw_s3_size_actual / 1024 / 1024 / 1024))       #Actual Usage of Bucket
+max=$(($radosgw_s3_size_max / 1024 / 1024 / 1024))          #Maximum Usage of Bucket (Quota)
+rem=$(($radosgw_s3_size_remaining / 1024 / 1024 / 1024))    #Remaining Usage of Bucket
 
 # calculating warn / crit
-maxten=$(($max / 10)) #warning at 10% of Maximum Usage
-maxtwen=$(($max / 20)) #critical 5% of Maximum Usage
+maxten=$(($max / 10))   #warning at 10% of Maximum Usage
+maxtwen=$(($max / 20))  #critical 5% of Maximum Usage
 
 # actual check ok/crit/warn
 if (($rem >= $maxten)); then
